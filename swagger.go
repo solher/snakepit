eadf6a1af42bc0c0bd7a949e559e5285ecb2aa57
Fabien Herfray <fabien.herfray@me.com>
2016-05-04 16:45:33 -0700

    Swagger middleware now serves dynamically.

--- swagger.go
+++ swagger.go
@@ -1,6 +1,8 @@
 package snakepit
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -8,27 +10,62 @@ import (
 	"golang.org/x/net/context"
 )
 
-type Swagger struct {
-	path string
+type swaggerConf struct {
+	Swagger     json.RawMessage `json:"swagger,omitempty"`
+	BasePath    string          `json:"basePath,omitempty"`
+	Schemes     []string        `json:"schemes,omitempty"`
+	Consumes    json.RawMessage `json:"consumes,omitempty"`
+	Produces    json.RawMessage `json:"produces,omitempty"`
+	Info        json.RawMessage `json:"info,omitempty"`
+	Paths       json.RawMessage `json:"paths,omitempty"`
+	Definitions json.RawMessage `json:"definitions,omitempty"`
+	Responses   json.RawMessage `json:"responses,omitempty"`
 }
 
-func NewSwagger() func(next chi.Handler) chi.Handler {
+type Swagger struct {
+	conf []byte
+}
+
+func NewSwagger(basePath, scheme string) func(next chi.Handler) chi.Handler {
 	path := "./swagger.json"
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		path = os.Getenv("HOME") + "/swagger.json"
 	}
 
-	swagger := &Swagger{path: path}
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	conf := &swaggerConf{}
+
+	if err := json.Unmarshal(buf, conf); err != nil {
+		panic(err)
+	}
+
+	conf.BasePath = basePath
+	conf.Schemes = []string{scheme}
+
+	raw, err := json.Marshal(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	swagger := &Swagger{conf: raw}
 	return swagger.middleware
 }
 
 func (rec *Swagger) middleware(next chi.Handler) chi.Handler {
 	return chi.HandlerFunc(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/swagger" {
+			w.WriteHeader(200)
+
 			w.Header().Add("Access-Control-Allow-Origin", "*")
 			w.Header().Add("Access-Control-Allow-Methods", "GET")
+			w.Header().Set("Content-Type", "application/json")
 
-			http.ServeFile(w, r, rec.path)
+			w.Write(rec.conf)
+
 			return
 		}
 
